3a10297faf928d4c68eb880bbe604ad1b9f5fed4
Ayush Jha <ayush@wasmer.io>
2024-05-14 10:26:23 +0545

    cuztomize config to suit my needs

--- config.def.h
+++ config.def.h
@@ -47,17 +47,17 @@
 /* The default command prefix key, when modified by cntrl.
  * This can be changed at runtime using the '-c' flag.
  */
-#define COMMAND_KEY 'g'
+#define COMMAND_KEY 'x'
 
 /* The change focus keys. */
-#define MOVE_UP         CODE(KEY_UP)
-#define MOVE_DOWN       CODE(KEY_DOWN)
-#define MOVE_RIGHT      CODE(KEY_RIGHT)
-#define MOVE_LEFT       CODE(KEY_LEFT)
+#define MOVE_UP         KEY(L'k')
+#define MOVE_DOWN       KEY(L'j')
+#define MOVE_RIGHT      KEY(L'l')
+#define MOVE_LEFT       KEY(L'h')
 #define MOVE_OTHER      KEY(L'o')
 
 /* The split terminal keys. */
-#define HSPLIT KEY(L'h')
+#define HSPLIT KEY(L'c')
 #define VSPLIT KEY(L'v')
 
 /* The delete terminal key. */
@@ -67,10 +67,10 @@
 #define BAILOUT KEY(L'c')
 
 /* clears the scrollback and everything */
-#define NUKE KEY(L'k')
+#define NUKE KEY(L'x')
 
 /* The force redraw key. */
-#define REDRAW KEY(L'l')
+#define REDRAW KEY(L'r')
 
 /* The scrollback keys. */
 #define SCROLLUP CODE(KEY_PPAGE)
